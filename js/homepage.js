378ff42601b53b7de7b95b4d5e765fd40a4b3461
Florent Benoit <Florent.Benoit@serli.com>
2014-06-06 15:53:33 +0200

    upload website

--- js/homepage.js
+++ js/homepage.js
@@ -1,6 +1,6 @@
-angular.module('homepage', [])
+angular.module('homepage', ['ngAnimate', 'ui.bootstrap', 'download-data'])
 
-  .config(function($provide) {
+  .config(function($provide, $locationProvider) {
     var pulseElements = $(),
         pulseColor = {r:0xE6, g:0xF0, b: 0xFF},
         baseColor = {r:0x99, g:0xc2, b: 0xFF},
@@ -44,6 +44,9 @@ angular.module('homepage', [])
          tick();
        }, 2000);
     });
+
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('!');
   })
 
   .value('indent', function(text, spaces) {
@@ -73,36 +76,49 @@ angular.module('homepage', [])
   })
 
   .value('escape', function(text) {
-    return text.replace(/\&/g, '&amp;').replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace(/"/g, '&quot;');
+    return text.replace(/\&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
   })
 
   .factory('script', function() {
 
     return {
-      angular: '<script src="http://code.angularjs.org/angular-' + angular.version.full + '.min.js"></script>\n',
-      resource: '<script src="http://code.angularjs.org/angular-resource-' + angular.version.full + '.min.js"></script>\n'
+      angular: '<script src="https://ajax.googleapis.com/ajax/libs/angularjs/' + angular.version.full + '/angular.min.js"></script>\n',
+      resource: '<script src="https://ajax.googleapis.com/ajax/libs/angularjs/' + angular.version.full + '/angular-resource.min.js"></script>\n',
+      route: '<script src="https://ajax.googleapis.com/ajax/libs/angularjs/' + angular.version.full + '/angular-route.min.js"></script>\n',
+      firebase: '<script src="https://cdn.firebase.com/v0/firebase.js"></script>\n    <script src="https://cdn.firebase.com/libs/angularfire/0.5.0/angularfire.min.js"></script>\n'
     };
   })
 
   .factory('fetchCode', function(indent) {
     return function get(id, spaces) {
-      return indent(angular.element(document.getElementById(id)).text(), spaces);
-    }
+      return indent(angular.element(document.getElementById(id)).html(), spaces);
+    };
   })
 
   .directive('code', function() {
     return {restrict: 'E', terminal: true};
   })
 
-  .directive('appRun', function(fetchCode, $templateCache) {
+  .directive('appRun', function(fetchCode, $templateCache, $browser) {
     return {
       terminal: true,
       link: function(scope, element, attrs) {
         var modules = [];
 
-        modules.push(function($provide) {
-          $provide.value('$templateCache', $templateCache);
-          $provide.value('$anchorScroll', angular.noop)
+        modules.push(function($provide, $locationProvider) {
+          $provide.value('$templateCache', {
+            get: function(key) {
+              var value = $templateCache.get(key);
+              if (value) {
+                value = value.replace(/\#\//mg, '/');
+              }
+              return value;
+            }
+          });
+          $provide.value('$anchorScroll', angular.noop);
+          $provide.value('$browser', $browser);
+          $locationProvider.html5Mode(true);
+          $locationProvider.hashPrefix('!');
         });
         if (attrs.module) {
           modules.push(attrs.module);
@@ -114,17 +130,18 @@ angular.module('homepage', [])
             event.preventDefault();
           }
         });
+        element.data('$injector', null);
         angular.bootstrap(element, modules);
       }
     };
   })
 
-  .directive('appSource', function(fetchCode, escape, script) {
+  .directive('appSource', function(fetchCode, escape, script, $compile, $timeout) {
     return {
       terminal: true,
+      scope: true,
       link: function(scope, element, attrs) {
         var tabs = [],
-            panes = [],
             annotation = attrs.annotate && angular.fromJson(fetchCode(attrs.annotate)) || {},
             TEMPLATE = {
               'index.html':
@@ -133,6 +150,8 @@ angular.module('homepage', [])
                 '  <head>\n' +
                 '    ' + script.angular +
                (attrs.resource ? ('    ' + script.resource.replace('></', '>\n    </')) : '') +
+               (attrs.route ? ('    ' + script.route.replace('></', '>\n   </')) : '') +
+               (attrs.firebase ? ('    ' + script.firebase) : '') +
                 '__HEAD__' +
                 '  </head>\n' +
                 '  <body>\n' +
@@ -141,24 +160,19 @@ angular.module('homepage', [])
                 '</html>'
           };
 
-    element.css('clear', 'both');
+        element.css('clear', 'both');
 
         angular.forEach(attrs.appSource.split(' '), function(filename, index) {
           var content;
 
-          tabs.push(
-            '<li class="' + (!index ? ' active' : '') + '">' +
-              '<a href="#' + id(filename) + '" data-toggle="tab">' + (index ? filename : 'index.html') + '</a>' +
-            '</li>');
-
-          if (index == 0) {
+          if (index === 0) {
             var head = [];
 
             angular.forEach(attrs.appSource.split(' '), function(tab, index) {
               var filename = tab.split(':')[0],
                   fileType = filename.split(/\./)[1];
 
-              if (index == 0) return;
+              if (index === 0) return;
               if (fileType == 'js') {
                 head.push('    <script src="' + filename + '"></script>\n');
               } else if (fileType == 'css') {
@@ -176,7 +190,7 @@ angular.module('homepage', [])
 
           // hack around incorrect tokenization
           content = content.replace('.done-true', 'doneTrue');
-          content = colourCode(content);
+          content = prettyPrintOne(escape(content), undefined, true);
           // hack around incorrect tokenization
           content = content.replace('doneTrue', '.done-true');
 
@@ -187,10 +201,14 @@ angular.module('homepage', [])
             var regexp = new RegExp('(\\W|^)(' + key.replace(/([\W\-])/g, '\\$1') + ')(\\W|$)');
 
             content = content.replace(regexp, function(_, before, token, after) {
-              var token = "__" + (counter++) + "__";
+              token = "__" + (counter++) + "__";
               popovers[token] =
-                '<code class="nocode" rel="popover" title="' + escape('<code>' + key + '</code>') +
-                '" data-content="' + escape(text) + '">' + escape(key) + '</code>';
+                '<span class="nocode"\n' +
+                '      popover-title="' + escape(key) + '"\n' +
+                '      popover-trigger="mouseenter"\n' +
+                '      popover-append-to-body="true"\n' +
+                '      popover-html-unsafe="' + escape(text) + '"><code>' + escape(key) + '</code>' +
+                '</span>';
               return before + token + after;
             });
           });
@@ -199,50 +217,83 @@ angular.module('homepage', [])
             content = content.replace(token, text);
           });
 
-          panes.push(
-            '<div class="tab-pane' + (!index ? ' active' : '') + '" id="' + id(filename) + '">' +
-              '<pre class="prettyprint linenums nocode">' + content +'</pre>' +
-            '</div>');
+          tabs.push(
+            '<tab heading="' + (index ? filename : 'index.html')  + '">\n' +
+            '  <pre class="prettyprint linenums nocode"><code>' + content +'</code></pre>\n' +
+            '</tab>\n'
+          );
         });
 
-        scope.$evalAsync(function() {
-          // must be delayed boucase of colourCode();
-          element.html(
-            '<div class="tabbable">' +
-              '<ul class="nav nav-tabs">' +
-                tabs.join('') +
-              '</ul>' +
-              '<div class="tab-content">' +
-                panes.join('') +
-              '</div>' +
-            '</div>');
-          element.find('[rel=popover]').popover().pulse();
-        });
+        element.html(
+          '<tabset>' +
+            tabs.join('') +
+          '</tabset>');
+        // element.find('[rel=popover]').popover().pulse();
 
-        function colourCode(html) {
-          // This function is here because the prettyPrint() has no API to color code a chunk of code without
-          // adding it to DOM.
-          var pre = $('<pre class="prettyprint linenums">');
-          pre.text(html);
-          $('body').append(pre);
-          prettyPrint();
-          pre.remove();
-          return pre.html();
-        }
-
-        function id(id) {
-          return id.replace(/\W/g, '-');
-        }
+        // Compile up the HTML to get the directives to kick-in
+        $compile(element.children())(scope);
+        $timeout(function() {
+          var annotationElements = element.find('span[popover-html-unsafe]');
+          $compile(annotationElements)(scope);
+        }, 0);
       }
-    }
+    };
   })
+.directive('codenvy1', function(fetchCode, escape, script) {
+  return {
+    terminal: true,
+    link: function(scope, element, attr) {
+      element.html(
+          '<a href="http://ide3.cf.codenvy-stg.com/factory?v=1.2&author=Florent+Benoit&vcs=git&projectattributes.pname=angularjs-tutorial-basics&projectattributes.ptype=AngularJS&openfile=index.html&vcsurl=https%3A%2F%2Fgithub.com%2Fbenoitf%2Fangularjs-tutorial-basics&action=openproject&image=http%3A%2F%2Ffactories.codenvy.com%2Fimages%2Fangular.jpg" target="_blank" class="btn btn-primary">' +
+            '<i class="icon-white icon-pencil"></i> ' +
+            'Edit Me' +
+          '</a>');
+    }
+  }
+})
+.directive('codenvy2', function(fetchCode, escape, script) {
+  return {
+    terminal: true,
+    link: function(scope, element, attr) {
+      element.html(
+          '<a href="http://ide3.cf.codenvy-stg.com/factory?v=1.2&author=Florent+Benoit&vcs=git&projectattributes.pname=angularjs-tutorial-some-control&projectattributes.ptype=AngularJS&openfile=index.html&vcsurl=https%3A%2F%2Fgithub.com%2Fbenoitf%2Fangularjs-tutorial-some-control&action=openproject&image=http%3A%2F%2Ffactories.codenvy.com%2Fimages%2Fangular.jpg" target="_blank" class="btn btn-primary">' +
+            '<i class="icon-white icon-pencil"></i> ' +
+            'Edit Me' +
+          '</a>');
+    }
+  }
+})
 
+.directive('codenvy3', function(fetchCode, escape, script) {
+  return {
+    terminal: true,
+    link: function(scope, element, attr) {
+      element.html(
+          '<a href="http://ide3.cf.codenvy-stg.com/factory?v=1.2&author=Florent+Benoit&vcs=git&projectattributes.pname=angularjs-tutorial-backend&projectattributes.ptype=AngularJS&openfile=index.html&vcsurl=https%3A%2F%2Fgithub.com%2Fbenoitf%2Fangularjs-tutorial-backend&action=openproject&image=http%3A%2F%2Ffactories.codenvy.com%2Fimages%2Fangular.jpg" target="_blank" class="btn btn-primary">' +
+            '<i class="icon-white icon-pencil"></i> ' +
+            'Edit Me' +
+          '</a>');
+    }
+  }
+})
+.directive('codenvy4', function(fetchCode, escape, script) {
+  return {
+    terminal: true,
+    link: function(scope, element, attr) {
+      element.html(
+          '<a href="http://ide3.cf.codenvy-stg.com/factory?v=1.2&author=Florent+Benoit&vcs=git&projectattributes.pname=angularjs-tutorial-create-component&projectattributes.ptype=AngularJS&openfile=index.html&vcsurl=https%3A%2F%2Fgithub.com%2Fbenoitf%2Fangularjs-tutorial-create-component&action=openproject&image=http%3A%2F%2Ffactories.codenvy.com%2Fimages%2Fangular.jpg" target="_blank" class="btn btn-primary">' +
+            '<i class="icon-white icon-pencil"></i> ' +
+            'Edit Me' +
+          '</a>');
+    }
+  }
+})
   .directive('jsFiddle', function(fetchCode, escape, script) {
     return {
       terminal: true,
       link: function(scope, element, attr) {
         var name = '',
-            stylesheet = '<link rel="stylesheet" href="http://twitter.github.com/bootstrap/assets/css/bootstrap.css">\n',
+            stylesheet = '<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/css/bootstrap-combined.min.css">\n',
             fields = {
               html: '',
               css: '',
@@ -253,7 +304,7 @@ angular.module('homepage', [])
           var fileType = file.split('.')[1];
 
           if (fileType == 'html') {
-            if (index == 0) {
+            if (index === 0) {
               fields[fileType] +=
                   '<div ng-app' + (attr.module ? '="' + attr.module + '"' : '') + '>\n' +
                     fetchCode(file, 2);
@@ -277,6 +328,8 @@ angular.module('homepage', [])
                stylesheet +
                script.angular +
                (attr.resource ? script.resource : '') +
+               (attr.route ? script.route : '') +
+               (attr.firebase ? script.firebase : '') +
                '<style>\n' +
                fields.css) +
             hiddenField('html', fields.html) +
@@ -297,38 +350,175 @@ angular.module('homepage', [])
   .directive('hint', function() {
     return {
       template: '<em>Hint:</em> hover over ' +
-          '<code class="nocode" rel="popover" title="Hover" ' +
-          'data-content="Place your mouse over highlited areas in the code for explanations.">me</code>.'
+          '<code class="nocode" popover-title="Hover" popover-trigger="mouseenter" popover-append-to-body="true"' +
+          'popover="Place your mouse over highlighted areas in the code for explanations.">me</code>.'
+    };
+  })
+
+  .controller('JumbotronCtrl', ['$scope', '$http', 'filterFilter', function($scope,   $http,   filterFilter) {
+
+    function byCategoryFilter(array, category) {
+      var results = [];
+      angular.forEach(array, function(video) {
+        if ( video.category == category ) {
+          results.push(video);
+        }
+      });
+      return results;
     }
-  })
 
-  .run(function($rootScope, startPulse){
-    $rootScope.version = angular.version;
-    $rootScope.$evalAsync(function(){
-      var videoModal = $('#videoModal');
+    var defaultCategory = 'basics';
+    $scope.category = defaultCategory;
 
-      videoModal.modal({show:false});
-      videoModal.on('shown', function() {
-        var iframe = videoModal.find('.modal-body').append('<iframe>').find('iframe');
-
-        iframe.attr({
-          width: 1280, height: 720, allowfullscreen: true,
-          src: 'http://www.youtube.com/embed/WuiHuZq_cg4?&autoplay=1'
-        });
-
-        // HACK: The only way I know of tricking YouTube to play HD is to show big and then resize.
-        setTimeout(function() {
-          iframe.attr({ width: 970, height: 556 });
-        }, 2500);
-      });
-      videoModal.on('hidden', function() {
-        videoModal.find('.modal-body').html('');
-      });
-
-      $('[rel=popover]').popover().pulse();
-      startPulse();
+    var allVideos;
+    $scope.loading = true;
+    $http.get('./featured-videos.json').success(function(results) {
+      $scope.loading = false;
+      allVideos = results;
+      $scope.filterByCategory($scope.category);
     });
+
+    $scope.filterBySearch = function(q) {
+      $scope.search = q;
+      $scope.category = null;
+      $scope.videos = filterFilter(allVideos, q);
+    };
+
+    $scope.filterByCategory = function(category) {
+      $scope.search = null;
+      $scope.category = category;
+      $scope.videos = byCategoryFilter(allVideos, category);
+    };
+  }])
+
+
+
+  .controller('AppCtrl', function($scope, $modal, BRANCHES) {
+    $scope.BRANCHES = BRANCHES;
+
+    $scope.showDownloadModal = function() {
+      $modal.open({
+        templateUrl: 'partials/download-modal.html',
+        windowClass: 'download-modal'
+      });
+    };
+
+    $scope.showVideo = function(videoUrl) {
+      $modal.open({
+        templateUrl: 'partials/video-modal.html',
+        windowClass: 'video-modal',
+        controller: 'VideoController',
+        resolve: {
+          videoUrl: function() { return videoUrl; }
+        }
+      });
+    };
+
   })
+
+  .controller('DownloadCtrl', function($scope, BRANCHES, BUILDS, DOWNLOAD_INFO) {
+
+    function getRelativeUrl(branch, build) {
+      switch (build.name) {
+        case 'Minified':
+          return branch.version + '/angular.min.js';
+        case 'Uncompressed':
+          return branch.version + '/angular.js';
+        case 'Zip':
+          return branch.version + '/angular-' + branch.version + '.zip';
+      }
+    }
+
+    $scope.BRANCHES = BRANCHES;
+    $scope.BUILDS = BUILDS;
+    $scope.DOWNLOAD_INFO = DOWNLOAD_INFO;
+
+    $scope.currentBranch = $scope.BRANCHES[0];
+    $scope.currentBuild = $scope.BUILDS[0];
+
+    $scope.setBranch = function(branch) {
+      $scope.currentBranch = branch;
+    };
+
+    $scope.setBuild = function(build) {
+      $scope.currentBuild = build;
+    };
+
+    var BASE_CDN_URL = 'https://ajax.googleapis.com/ajax/libs/angularjs/';
+    $scope.cdnUrl = function() {
+      if ($scope.currentBuild.name !== 'Zip') {
+        return BASE_CDN_URL + getRelativeUrl($scope.currentBranch, $scope.currentBuild);
+      }
+    };
+
+
+    var BASE_CODE_ANGULAR_URL = 'http://code.angularjs.org/';
+    $scope.downloadLink = function() {
+      return $scope.cdnUrl() || BASE_CODE_ANGULAR_URL + getRelativeUrl($scope.currentBranch, $scope.currentBuild);
+    };
+
+
+  })
+
+.controller('VideoController', function($scope, $timeout, $sce, videoUrl) {
+
+  // HACK: Trick YouTube to play HD by showing big and then resizing after 2500ms.
+
+  $scope.video = {
+    width: 1280,
+    height: 720,
+    allowfullscreen: true,
+    src: $sce.trustAsResourceUrl(videoUrl)
+  };
+
+  $timeout(function() {
+    $scope.video.width = 970;
+    $scope.video.height = 556;
+  }, 2500);
+
+})
+
+
+// Angular UI Bootstrap provide some excellent directives, but the popover didn't allow for HTML content
+// The popoverHtmlUnsafe and popoverHtmlUnsafePopup implement this on top of the AngularUI Bootstrap's $tooltip service
+.directive( 'popoverHtmlUnsafePopup', function ($templateCache) {
+
+  $templateCache.put("template/popover/popover-html-unsafe-popup.html",
+    "<div class=\"popover {{placement}}\" ng-class=\"{ in: isOpen(), fade: animation() }\">\n" +
+    "  <div class=\"arrow\"></div>\n" +
+    "\n" +
+    "  <div class=\"popover-inner\">\n" +
+    "      <h3 class=\"popover-title\" ng-bind=\"title\" ng-show=\"title\"></h3>\n" +
+    "      <div class=\"popover-content\" bind-html-unsafe=\"content\"></div>\n" +
+    "  </div>\n" +
+    "</div>\n" +
+    "");
+
+  return {
+    restrict: 'EA',
+    replace: true,
+    scope: { title: '@', content: '@', placement: '@', animation: '&', isOpen: '&' },
+    templateUrl: 'template/popover/popover-html-unsafe-popup.html'
+  };
+})
+
+.directive( 'popoverHtmlUnsafe', [ '$compile', '$timeout', '$parse', '$window', '$tooltip', function ( $compile, $timeout, $parse, $window, $tooltip ) {
+  return $tooltip( 'popoverHtmlUnsafe', 'popover', 'click' );
+}])
+
+
+
+.run(function($rootScope, startPulse){
+  $rootScope.version = angular.version;
+
+  //   $('[rel=popover]').
+  //     popover().
+  //     pulse();
+  //   startPulse();
+  // });
+
+});
+
 
 angular.module('Group', ['ngResource']);
 
@@ -339,5 +529,4 @@ function GroupCtrl($scope, $resource)
 
   $scope.recommendedGroups = $resource('groups/index/getrecommended');
   $scope.recommendedGroups.get();
-
 }
